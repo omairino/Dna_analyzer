0c261371fb18139d2166a0faddd47d924ec13d48
Administrator <omair.issa6@gmail.com>
2018-12-25 12:46:02 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/excellenteam_erez_c_dna_analyzer_omairino.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/excellenteam_erez_c_dna_analyzer_omairino.dir/CONTROLLER/main.cpp.obj"
+  "excellenteam_erez_c_dna_analyzer_omairino.pdb"
+  "excellenteam_erez_c_dna_analyzer_omairino.exe"
+  "excellenteam_erez_c_dna_analyzer_omairino.exe.manifest"
+  "libexcellenteam_erez_c_dna_analyzer_omairino.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/excellenteam_erez_c_dna_analyzer_omairino.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/excellenteam_erez_c_dna_analyzer_omairino.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for excellenteam_erez_c_dna_analyzer_omairino.
+# This may be replaced when dependencies are built.
