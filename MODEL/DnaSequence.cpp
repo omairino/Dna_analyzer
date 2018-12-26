5777e973f98d63af93d08980e3b3f6f4bac27099
Administrator <omair.issa6@gmail.com>
2018-12-26 18:45:45 +0200

    Class Nucleotide Done!

--- MODEL/DnaSequence.cpp
+++ MODEL/DnaSequence.cpp
@@ -1,5 +1,27 @@
-//
-// Created by Administrator on 12/26/2018.
-//
-
 #include "DnaSequence.h"
+
+Nucleotide::Nucleotide(const char nucleotide) {
+    if ((nucleotide != 'A') && (nucleotide != 'G') && (nucleotide != 'C') && (nucleotide != 'T')) {
+        throw std::invalid_argument("invalid Nucleotide");
+    }
+    m_nucleotide = nucleotide;
+}
+
+Nucleotide::Nucleotide() {}
+
+Nucleotide::~Nucleotide() {}
+
+char Nucleotide::getNucleotide() {
+    return m_nucleotide;
+}
+
+Nucleotide &Nucleotide::operator=(const Nucleotide &other) {
+    this->m_nucleotide = other.m_nucleotide;
+    return *this;
+}
+
+std::ostream &operator<<(std::ostream &os, const Nucleotide &dt) {
+    os << dt.m_nucleotide;
+    return os;
+}
+
